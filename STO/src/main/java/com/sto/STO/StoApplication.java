822f60870d15975322e0d9b180f52c5a547d225a
Ketan Mirg <ketan.mirg@synapse.com>
2017-09-12 12:16:50 +0530

    Repo creation for STO

---
+++ STO/src/main/java/com/sto/STO/StoApplication.java
@@ -0,0 +1,12 @@
+package com.sto.STO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoApplication.class, args);
+	}
+}
